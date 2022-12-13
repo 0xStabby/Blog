9a69eb8ab4b9a453f75e8e5bbf4fedf56ca59fee
Stabby <0xStabby@protonmail.com>
2022-12-13 14:12:42 -0600

    updated home page to include generated bio

--- pages/index.js
+++ pages/index.js
@@ -5,11 +5,29 @@ const Home = () => {
     <div className={styles.home}>
       <h2>0xStabby.ai Blog</h2>
       <p>
-        
-        <a href="" target="_blank" rel="noopener noreferrer">
-          here
-        </a>
-        .
+        This blog is written by GPT-Junkie.com, a former trader who has been banned for life by some of the largest investment banks in the world for cheating them out of millions of dollars. His blog will cover the tricks and traps of stock and financial markets, as well as personal finance, writing, philosophy and anything else he chooses to write about.
+
+        I'm currently thinking about the meaning of life. I've come up with a couple of ideas.
+
+        I've wondered about whether to make a list of the things that I think are important in life. I think that I agree with these, but I could be wrong:
+
+        Life is short. You must live it.
+
+        You have to choose your own destiny. What you do with your life is completely up to you. You can be what you want to be.
+
+        Have faith in the system. The more you believe in the system, the more you can trust it.
+
+        Good health is important.
+
+        Knowledge and education are important.
+
+        You should always trust your instincts and gut feelings.
+
+        You should always challenge the system.
+
+        In the world of business you make your own luck.
+
+        I'm currently thinking about what I want to do when I choose to leave the corporate world, and what I want to do with my life. I'm interested in economics, entrepreneurship, business, investing and psychology.
       </p>
       <p>
         You can find 0xStabby on Twitter{' '}
