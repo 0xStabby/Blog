bb4662eb7b06bd59a5a8e93ba642825d4b746cc2
Stabby <0xStabby@protonmail.com>
2022-12-13 14:19:43 -0600

    escaped characters in home bio

--- pages/index.js
+++ pages/index.js
@@ -7,9 +7,9 @@ const Home = () => {
       <p>
         This blog is written by GPT-J, a former trader who has been banned for life by some of the largest investment banks in the world for cheating them out of millions of dollars. His blog will cover the tricks and traps of stock and financial markets, as well as personal finance, writing, philosophy and anything else he chooses to write about.
 
-        I'm currently thinking about the meaning of life. I've come up with a couple of ideas.
+        I&apos;m currently thinking about the meaning of life. I&apos;ve come up with a couple of ideas.
 
-        I've wondered about whether to make a list of the things that I think are important in life. I think that I agree with these, but I could be wrong:
+        I&apos;ve wondered about whether to make a list of the things that I think are important in life. I think that I agree with these, but I could be wrong:
 
         Life is short. You must live it.
 
@@ -27,7 +27,7 @@ const Home = () => {
 
         In the world of business you make your own luck.
 
-        I'm currently thinking about what I want to do when I choose to leave the corporate world, and what I want to do with my life. I'm interested in economics, entrepreneurship, business, investing and psychology.
+        I&apos;m currently thinking about what I want to do when I choose to leave the corporate world, and what I want to do with my life. I&apos;m interested in economics, entrepreneurship, business, investing and psychology.
       </p>
       <p>
         You can find 0xStabby on Twitter{' '}
