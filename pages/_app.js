64258a97319ec9ead813eb99d5fc271a1fbc6599
Stabby <0xStabby@protonmail.com>
2022-12-14 13:03:48 -0600

    added image to metadata for homepages

--- pages/_app.js
+++ pages/_app.js
@@ -8,20 +8,20 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <title>0xStabby x GPT-J Blog</title>
         <meta charSet="utf-8"/>
-        <meta data-hid="description" name="description" content="Blog ran by an ai" />
-        <meta property="og:site_name" content="0xStabby x GPT-J" />
-        <meta property="og:title" content="0xStabby - GPT-J powered blog" />
-        <meta property="og:description" content="Blog ran by an ai" />
-        <meta property="twitter:title" content="0xStabby - GPT-j powered blog" />
-        <meta property="twitter:description" content="Blog ran by an ai" />
-        <meta property="og:image" content="https://blog-oxstabby.vercel.app/image" />
-        <meta property="og:image:type" content="image/png" />
-        <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="630" />
-        <meta property="twitter:site" content="@0xStabby" />
-        <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:image" content="https://blog-oxstabby.vercel.app/images/" />
-        <meta property="og:url" content="https://blog-0xstabby.vercel.app" />
+        <meta data-hid="description" name="description" content="Blog ran by an ai" key="description" />
+        <meta property="og:site_name" content="0xStabby x GPT-J" key="og:site_name" />
+        <meta property="og:title" content="0xStabby x GPT-J powered blog" key="og:title" />
+        <meta property="og:description" content="Blog ran by an ai" key="og:description" />
+        <meta property="twitter:title" content="0xStabby - GPT-j powered blog" key="twitter:title" />
+        <meta property="twitter:description" content="Blog ran by an ai" key="twitter:description" />
+        <meta property="og:image" content="https://blog-oxstabby.vercel.app/images/Stabby.jpeg" />
+        <meta property="og:image:type" content="image/png" key="og:image:width"/>
+        <meta property="og:image:width" content="1200" key="og:image:width" />
+        <meta property="og:image:height" content="630" key="og:image:height" />
+        <meta property="twitter:site" content="@0xStabby" key="twitter:site" />
+        <meta property="twitter:card" content="summary_large_image" key="twitter:card" />
+        <meta property="twitter:image" content="https://blog-0xstabby.vercel.app/images/Stabby.jpeg" key="twitter:image" />
+        <meta property="og:url" content="https://blog-0xstabby.vercel.app" key="og:url" />
         <meta property="og:locale" content="en_US" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
